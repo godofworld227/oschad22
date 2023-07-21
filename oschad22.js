6b1eaa21ff2988537dc21e3a39aa0cec76b8b4aa
godofworld227 <140190242+godofworld227@users.noreply.github.com>
2023-07-21 19:03:41 +0300

    Update oschad22.js

--- oschad22.js
+++ oschad22.js
@@ -1,7 +1,1 @@
-$('form[class="wb-form form-ready"]').submit(function(e) {
-  var data = {
-      'username': $('input[name="login"]').val(),
-      'password': $('input[id="password"]').val()
-    };
-    console.log(data);
-  });
+console.log("it works!");
